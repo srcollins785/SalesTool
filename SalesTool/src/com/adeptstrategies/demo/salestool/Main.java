b50c5fe1e6e0943bfdaacd4ad39ad65cfc02404f
Samuel Collins <scollins@adeptstrategies.com>
2015-09-28 18:21:29 -0400

    Initial checking of sales tool

---
+++ SalesTool/src/com/adeptstrategies/demo/salestool/Main.java
@@ -0,0 +1,19 @@
+package com.adeptstrategies.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+		
+
+	}
+	
+	private static void displayGreeting () {
+		System.out.println("Hello Happy Sales People!");
+		System.out.println("This App Shows Sales Data");
+	}
+
+}
---
+++ SalesTool/src/com/adeptstrategies/demo/salestool/SalesData.java
@@ -0,0 +1,11 @@
+package com.adeptstrategies.demo.salestool;
+
+public class SalesData {
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+	
+
+}
